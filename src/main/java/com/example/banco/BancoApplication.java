5bd120e84027acc4602f4f4651cff5d76914d395
SergioEstebanPi <sepi_147@hotmail.com>
2019-08-11 16:42:23 -0500

    Agrego ejemplo de registro de cliente

--- src/main/java/com/example/banco/BancoApplication.java
+++ src/main/java/com/example/banco/BancoApplication.java
@@ -1,9 +1,8 @@
 package com.example.banco;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
 
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -11,18 +10,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.banco.model.Client;
-import com.example.banco.model.ClientDTO;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 public class BancoApplication {
@@ -52,9 +47,28 @@ public class BancoApplication {
 
 			ResponseEntity<Client> result = restTemplate.getForEntity(urlClient, Client.class);
 			if (result.getStatusCode() == HttpStatus.OK) {
-				System.out.println("INI");
 				Client clientes = result.getBody();
 				System.out.println(clientes.getProperties());
+				
+				System.out.println("Test de insercion");
+				
+				JSONObject clienteNuevo = new JSONObject();
+				clienteNuevo.put("identification", "01234");
+				clienteNuevo.put("firstname", "newcl");
+				clienteNuevo.put("lastname", "lasnewcl");
+				clienteNuevo.put("birthdate", "2019-01-01");
+				
+				System.out.println("PETICION: " + clienteNuevo.toString());
+				 HttpEntity<String> request = new HttpEntity<String>(clienteNuevo.toString(), headers);
+				ResponseEntity<Client> result2 = restTemplate.postForEntity(urlClient, request, Client.class);
+				if(result2.getStatusCode() == HttpStatus.OK) {
+					System.out.println("cliente registrado correctamente");
+					System.out.println(result2.getBody());
+					System.out.println(result2.getBody().getProperties());
+				} else {
+					System.out.println("error al registrar el cliente");
+				}
+				
 			} else {
 				System.out.println("Error al consumir el servicio");
 			}
